1e99c3e6fac5674b822158bfc34db0f0e131e578
Nevalyashka1337 <ivan.nedelyuk@yandex.ru>
2019-03-09 12:23:58 +0300

    add main reducer and test for it

---
+++ client/src/reducer.js
@@ -0,0 +1,12 @@
+
+export const initState = {
+	online: 0
+}
+
+const reducer = ( state = initState, action ) => {
+	switch (action.type) {
+		default:
+		return state
+	}
+}
+export default reducer
