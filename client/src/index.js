1d79dc1360676d30307cdbf1cfdf37884f5e5bfd
Nevalyashka1337 <ivan.nedelyuk@yandex.ru>
2019-03-09 13:05:33 +0300

    add store and Provider

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/app'
 import * as serviceWorker from './serviceWorker'
+import store from './store'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+, document.getElementById('root'))
 
 if ( process.env.NODE_ENV === 'development' ) serviceWorker.unregister();
 else if ( process.env.NODE_ENV === 'production' ) serviceWorker.register()
---
+++ client/src/store.js
@@ -0,0 +1,7 @@
+import { createStore } from 'redux'
+import { devToolsEnhancer  } from 'redux-devtools-extension/logOnlyInProduction'
+import reducer from './reducer'
+
+const store = createStore(reducer, devToolsEnhancer())
+
+export default store
