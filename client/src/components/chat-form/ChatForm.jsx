c87c298d739f343c92507a7d2b51b1c9d3242982
Nevalyashka1337 <ivan.nedelyuk@yandex.ru>
2019-03-09 22:14:20 +0300

    add chat-form

---
+++ client/src/components/chat-form/ChatForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ChatForm() {
+	return (
+		<div className="chat-form">
+			<form>
+				<input type="text" placeholder="write a message..."/>
+			</form>
+		</div>
+	)
+}
+
+export default ChatForm
