0bbd746f1f221685d25cdb369ff2d2b359d9c1ef
Nevalyashka1337 <ivan.nedelyuk@yandex.ru>
2019-03-09 21:57:40 +0300

    add chat layout

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import './Chat.css'
 
 function Chat(props) {
 	return (
-		<div>
-			<p>Chat</p>
+		<div className="chat-wrapper">
+			<div className="chat-pager">
+				<div className="chat-header">
+					<span className="status-dot"></span>
+					<p>3 online</p>
+				</div>
+				<div className="chat-body">
+					<div className="chat-display">
+						<p>msg-1</p><br/>
+						<p>msg-2</p><br/>
+						<p>msg-3</p><br/>
+						<p>msg-4</p><br/>
+						<p>msg-5</p><br/>
+					</div>
+					<div className="chat-form">
+						<form>
+							<input type="text" placeholder="write a message..."/>
+						</form>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
