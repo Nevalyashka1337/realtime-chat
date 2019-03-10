15fd61f792c7b02405e3fc98afd89f212e589221
Nevalyashka1337 <ivan.nedelyuk@yandex.ru>
2019-03-10 13:11:39 +0300

    add send messega and online status from server

--- server/index.js
+++ server/index.js
@@ -4,8 +4,31 @@ const PORT = 1337
 
 const wss = new WebSocket.Server({ port: PORT })
 
+const send = (type, msg) => {
+	wss.clients.forEach(client => {
+		if ( client.readyState === WebSocket.OPEN ) {
+			client.send(JSON.stringify({
+				type: type,
+				payload: msg
+			}))
+		}
+	})
+}
+
 wss.on('connection', ws => {
-	console.log('new connection')
+	
+	send('UPDATE_ONLINE', wss.clients.size)
+	
+	ws.on('message', msg => {
+		if ( typeof msg === 'string' && msg.length > 0 && msg.length <= 100 ){
+			send('NEW_MESSAGE', {
+				id: "" + new Date().getTime() + new Date().getMilliseconds(),
+				text: msg
+			})
+		}
+	})
+
+	ws.on('close', () => send('UPDATE_ONLINE', wss.clients.size))
 })
 
 wss.on('listening', () => console.log(`wss is running on port: ${PORT}`))
