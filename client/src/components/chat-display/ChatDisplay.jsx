ec364cec18ef92c734b5b795d76e965c0dc315ce
Nevalyashka1337 <ivan.nedelyuk@yandex.ru>
2019-03-10 13:12:54 +0300

    add scroll to bottom after update

--- client/src/components/chat-display/ChatDisplay.jsx
+++ client/src/components/chat-display/ChatDisplay.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-function ChatDisplay({ messageList }) {
-	return (
-		<div className="chat-display">
-			{messageList.map(({ text, id }) => {
-				return (
-					<React.Fragment key={id}>
-						<p>{text}</p><br/>
-					</React.Fragment>
-				)
-			})}
-		</div>
-	)
+class ChatDisplay extends React.Component {
+	
+	componentDidUpdate() {
+		this.scrollToBottom()
+	}
+	
+	scrollToBottom = () => {
+		this.messagesEnd.scrollIntoView({ behavior: 'smooth' })
+	}
+
+	render() {
+		return (
+			<div className="chat-display">
+				{this.props.messageList.map(({ text, id }) => {
+					return (
+						<React.Fragment key={id}>
+							<p>{text}</p><br/>
+						</React.Fragment>
+					)
+				})}
+				<div style={{ float:"left", clear: "both" }}
+					ref={el => this.messagesEnd = el }>
+        </div>
+			</div>
+		)
+	}
 }
 
 const mapStateToProps = state => ({
