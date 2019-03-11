bc083a084b92f0f60c152bbe93c199c3eb1c0339
Nevalyashka1337 <ivan.nedelyuk@yandex.ru>
2019-03-11 23:04:43 +0300

    delete service worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/app'
-import * as serviceWorker from './serviceWorker'
 import store from './store'
 import { Provider } from 'react-redux'
 
@@ -10,6 +9,3 @@ ReactDOM.render(
 		<App />
 	</Provider>
 , document.getElementById('root'))
-
-if ( process.env.NODE_ENV === 'development' ) serviceWorker.unregister();
-else if ( process.env.NODE_ENV === 'production' ) serviceWorker.register()
